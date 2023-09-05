d92524ac9d2d8aed5e841c9727d9babeacacf09e
joao-eduardo17 <125677846+joao-eduardo17@users.noreply.github.com>
2023-09-05 14:07:19 -0300

    algumas mudanças e termino do sobre
    adição das tecnologias da página Sobre, adição de um GIF na Home e mudanças na lógica base

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,20 @@
-import * as decimau from "./oi";
 //Decimal para binário
-
-decimau.decimal_binario(1010)   
+function decimal_binario(n){
+    let r = '';
+    let s = '';
+    if(n == 0){
+        return n
+    }
+    while(n > 1){
+        r += String(n%2)
+        n = Math.floor(n/2)
+    }
+    r = r.split('');
+    r = r.reverse();
+    r.forEach((e) => s+=e)
+    s = '1' + s
+    return s
+}
 
 
 //Binário para decimal
