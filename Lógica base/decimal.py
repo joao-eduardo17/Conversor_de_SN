5b27a28d75bfa4bca8c222a819e72be1193f7984
joao-eduardo17 <125677846+joao-eduardo17@users.noreply.github.com>
2023-09-07 14:56:23 -0300

    implementação do hexadecimal
    implementação do sistema Hexadecimal em python e o começo da lógica em JavaScript

--- Lógica base/decimal.py
+++ Lógica base/decimal.py
@@ -1,5 +1,5 @@
 #Decimal para binário
-def binário(n):
+def binario(n):
     resultado = ''
     if n == 0:
         return n
@@ -11,22 +11,43 @@ def binário(n):
 #Deciaml para octal
 def octal(n):
     resultado = ''
-    if n <= 8:
+    if n < 8:
         return n
-    while n > 8:
+    while n >= 8:
         resultado += str(n%8)
         n = n//8
     x = str(n) + str(resultado[::-1])
     return int(x)
 
 #Decimal para hexadecimal
-# def hexa(n):
-#     resultado = ''
-#     if n <= 16:
-#         return n
-#     while n > 16:
-#         x = n % 16
-#         if x > 9:
-#             match (x):
-#                 case 11:
-#                      return
+def hexa(n):
+    resultado = ''
+    letras = 'a b c d e f'.split()
+    index = 10
+    if n <= 9:
+        return n
+    if n > 9 and n <= 15:
+        for c in range(6):
+            if n == index:
+                print(letras[c])
+                return
+            index += 1
+    while n > 15:
+        x = n % 16
+        if x > 9:
+            index = 10
+            for k in range(6):
+                if x == index:
+                    resultado += letras[k]
+                index += 1
+        else:
+            resultado += str(x)
+        n = n//16
+    if n > 9:
+        index = 10
+        for p in range(6):
+            if n == index:
+                n = letras[p]
+            index += 1
+    print(str(n) + resultado[::-1])
+    return
--- scripts/decimal.js
+++ scripts/decimal.js
@@ -10,19 +10,75 @@ function binario(n){
         n = Math.floor(n/2);
     }
     valor = valor.split('');
-    valor = valor.reverse();
-    valor.ForEach((e) => resultado+=e);
+    valor.forEach((e) => resultado+=e);
     resultado = '1' + resultado;
     return resultado
 }
 
 //Decimal para octal
 function octal(n){
-    return
+    let resultado = '';
+    let valor = '';
+    if(n < 8){
+        return n;
+    }
+    while(n >= 8){
+        valor += String(n%8);
+        n = Math.floor(n/8);
+    }
+    valor = valor.split('');
+    valor.forEach((e) => resultado+=e);
+    resultado = String(n) + String(resultado);
+    return resultado
+
 }
 
 
 //Decimal para Hexadecimal
 function hexa(n){
-    return
+    const letras = 'a b c d e f'.split('');
+    let resultado = '';
+    let valor = '';
+    let index = 10;
+    let divisão;
+    if(n<=9){
+        return n
+    }
+    if(n > 9 && n <= 15){
+        for(let c = 0; c < 12 ; c+=2){
+            if(n == index){
+                return letras[c]
+            }
+            index++;
+        }
+    }
+    while(n > 15){
+        divisão = n % 16;
+        if(divisão > 9){
+            index = 10;
+            for(let k = 0; k < 12; k+=2){
+                if(divisão == index){
+                    valor += letras[k];
+                }
+                index++;
+            }
+        }else{
+            valor += String(divisão);
+        }
+        n = Math.floor(n/16);
+    }
+    if(n > 9){
+        index = 10;
+        for(let p = 0; p < 12; p+=2){
+            if(n == index){
+                n = letras[p];
+            }
+            index++;
+        }
+    }
+    valor = valor.split('');
+    console.log(valor)
+    valor.forEach((e) => resultado+=e)
+    console.log(resultado)
+    return String(n) + resultado
 }
