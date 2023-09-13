e553c746d5fd06810a50aeb7e692d16afbd463a0
joao-eduardo17 <125677846+joao-eduardo17@users.noreply.github.com>
2023-09-12 21:10:21 -0300

    Correção
    Correção do Octal para Hexadecimal em JS e Python

--- scripts/octal.js
+++ scripts/octal.js
@@ -26,7 +26,7 @@ function octalToDecimal(n){
     return resultado   
 }
 
-//Octal para hexadecimal //manutenção//
+//Octal para hexadecimal 
 function octalToHexa(n){
     n = String(n).split('');
     let resultado = '';
@@ -38,5 +38,39 @@ function octalToHexa(n){
             }
         }
     })
-    n = String(resultado)
+    n = String(parseInt(resultado));
+    resultado = '';
+    let contador;
+    let contador2;
+    const letras = ['a','b','c','d','e','f'];
+    const numerosHexa = ['0000', '0001', '0010', '0011', '0100', '0101', '0110', '0111', '1000', '1001', '1010', '1011', '1100', '1101', '1110', '1111'];
+    switch(n.length % 4){
+        case 1:
+            n = '000' + n;
+            break;
+        case 2:
+            n = '00' + n;
+            break;
+        case 3:
+            n = '0' + n;
+    }
+    for(let c = 0; c < n.length; c += 4){
+        contador = 0;
+        index = n.slice(c,c+4);
+        numerosHexa.forEach((e) => {
+            if(index == e && contador > 9){
+                contador2 = 10;
+                letras.forEach((k) => {
+                    if(contador == contador2){
+                        resultado += k;
+                    }
+                    contador2++;
+                })
+            }else if(index == e){
+                resultado += String(contador);
+            }
+            contador++
+        })
+    }
+    return resultado
 }
